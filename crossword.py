9165d055afbe6bbf376af6e60cc6ca85eb975332
Spartansold <71949150+Spartansold@users.noreply.github.com>
2022-12-16 19:07:49 -0500

    Add files via upload

---
+++ crossword.py
@@ -0,0 +1,132 @@
+
+
+class Variable():
+
+    ACROSS = "across"
+    DOWN = "down"
+
+    def __init__(self, num, direction, length):
+        """Create a new variable with starting point, direction, and length."""
+        self.num = num
+        self.direction = direction
+        self.length = length
+        self.cells = []
+        for k in range(self.length):
+            self.cells.append(
+                (self.num + (k if self.direction == Variable.DOWN else 0),
+                 self.num + (k if self.direction == Variable.ACROSS else 0))
+            )
+
+    def __hash__(self):
+        return hash((self.num, self.direction, self.length))
+
+    def __eq__(self, other):
+        return (
+            (self.num == other.num) and
+            (self.direction == other.direction) and
+            (self.length == other.length)
+        )
+
+    def __str__(self):
+        return f"({self.num}, {self.direction} : {self.length}"
+
+    def __repr__(self):
+        direction = repr(self.direction)
+        return f"Variable({self.num}, {direction}, {self.length})"
+
+
+class Crossword():
+
+    def __init__(self, structure_file, words_file):
+
+        # Determine structure of crossword
+        with open("data/"+structure_file) as f:
+            contents = f.read().splitlines()
+            self.height = len(contents)
+            self.width = max(len(line) for line in contents)
+
+            self.structure = []
+            for i in range(self.height):
+                row = []
+                for j in range(self.width):
+                    if j >= len(contents[i]):
+                        row.append(False)
+                    elif contents[i][j] == "#":
+                        row.append(False)
+                    else:
+                        row.append(True)
+                self.structure.append(row)
+
+        # Save vocabulary list
+        with open("data/"+words_file) as f:
+            self.words = set(f.read().upper().splitlines())
+
+        # Determine variable set
+        self.variables = set()
+        for i in range(self.height):
+            for j in range(self.width):
+
+                # Vertical words
+                starts_word = (
+                    contents[i][j].isdigit() and (i == 0 or not self.structure[i - 1][j])
+                )
+                if starts_word:
+                    length = 1
+                    for k in range(i + 1, self.height):
+                        if self.structure[k][j]:
+                            length += 1
+                        else:
+                            break
+                    if length > 1:
+                        self.variables.add(Variable(
+                            num=int(contents[i][j]),
+                            direction=Variable.DOWN,
+                            length=length
+                        ))
+
+                # Horizontal words
+                starts_word = (
+                    contents[i][j].isdigit() and (j == 0 or not self.structure[i][j - 1])
+                )
+                if starts_word:
+                    length = 1
+                    for k in range(j + 1, self.width):
+                        if self.structure[i][k]:
+                            length += 1
+                        else:
+                            break
+                    if length > 1:
+                        self.variables.add(Variable(
+                            num=int(contents[i][j]),
+                            direction=Variable.ACROSS,
+                            length=length
+                        ))
+
+        # Compute overlaps for each word
+        self.overlaps = dict()
+        for v1 in self.variables:
+            for v2 in self.variables:
+                if v1 == v2:
+                    continue
+                cells1 = v1.cells
+                cells2 = v2.cells
+                intersection = set(cells1).intersection(cells2)
+                if not intersection:
+                    self.overlaps[v1, v2] = None
+                else:
+                    intersection = intersection.pop()
+                    self.overlaps[v1, v2] = (
+                        cells1.index(intersection),
+                        cells2.index(intersection)
+                    )
+
+    def neighbors(self, var):
+        """Given a variable, return set of overlapping variables."""
+        return set(
+            v for v in self.variables
+            if v != var and self.overlaps[v, var]
+        )
+
+    def remove(self, var):
+        if var in self.words:
+            self.words.remove(var)
